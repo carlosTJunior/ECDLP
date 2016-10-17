33f5e4d2b3aaa8a1e44fd4524d8b35f84cf2adc6
carlosTJunior <carlosjunior117@hotmail.com>
2016-10-17 14:55:25 -0200

    Changing point to work with long instead GMP

--- point.c
+++ point.c
@@ -5,34 +5,21 @@
 Point* point_alloc()
 {
     Point* P = (Point*) malloc(sizeof(Point));
-    mpz_init(P->x);
-    mpz_init(P->y);
+    //mpz_init(P->x);
+    //mpz_init(P->y);
 
     return P;
 }
 
-int point_init(Point* P, const char* x, const char* y)
+int point_init(Point* P, const BigInt x, const BigInt y)
 {
     if(P == NULL) {
         fprintf(stderr, "ERROR: point_create() -> invalid pointer\n");
         exit(1);
     }
-    mpz_set_str(P->x, x, 10);
-    mpz_set_str(P->y, y, 10);
 
-    return 0;
-}
-
-int point_init_mpz_t(Point* P, const mpz_t x, const mpz_t y)
-{
-    if(P == NULL) {
-        fprintf(stderr, \
-                "ERROR: point_create_mpz_t() -> invalid pointer\n");
-        return -1;
-    }
-    mpz_set(P->x, x);
-    mpz_set(P->y, y);
-
+    P->x = x;
+    P->y = y;
     return 0;
 }
 
@@ -43,8 +30,8 @@ int point_at_infinity(Point *P)
                 "ERROR: point_at_infinity() -> invalid pointer\n");
         return -1;
     }
-    mpz_set_si(P->x, -1);
-    mpz_set_si(P->y, -1);
+    P->x = -1;
+    P->y = -1;
 
     return 0;
 }
@@ -52,7 +39,7 @@ int point_at_infinity(Point *P)
 int point_is_equal(const Point* P, const Point* Q)
 {
     int is_equal = 0;
-    if(mpz_cmp(P->x, Q->x) == 0 && mpz_cmp(P->y, Q->y) == 0)
+    if((P->x == Q->x) && (P->y == Q->y))
         is_equal = 1;
 
     return is_equal;
@@ -64,15 +51,15 @@ int point_copy(Point* self, const Point* other)
         fprintf(stderr, "ERROR: point_copy() -> invalid pointer\n");
         return -1;
     }
-    mpz_set(self->x, other->x);
-    mpz_set(self->y, other->y);
+    self->x = other->x;
+    self->y = other->y;
     
     return 0;
 }
 
 void point_destroy(Point* P)
 {
-    mpz_clear(P->x);
-    mpz_clear(P->y);
+    //mpz_clear(P->x);
+    //mpz_clear(P->y);
     free(P);
 }
--- point.h
+++ point.h
@@ -1,16 +1,17 @@
 #ifndef _POINT
 #define _POINT
 
-#include <gmp.h>
+//#include <gmp.h>
+
+#define BigInt long
 
 typedef struct _point {
-    mpz_t x;
-    mpz_t y;
+    BigInt x;
+    BigInt y;
 } Point;
 
 extern Point* point_alloc();
-extern int point_init(Point*, const char*, const char*);
-extern int point_init_mpz_t(Point*, const mpz_t, const mpz_t);
+extern int point_init(Point*, const BigInt, const BigInt);
 extern int point_at_infinity(Point*);
 extern int point_is_equal(const Point*, const Point*);
 extern int point_copy(Point*, const Point*);
